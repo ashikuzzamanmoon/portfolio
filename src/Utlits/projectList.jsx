74cae69a4328dced2b7aa27770561d405ba4d80c
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2025-10-13 23:25:23 +0600

    add project image

--- src/Utlits/projectList.jsx
+++ src/Utlits/projectList.jsx
@@ -1,17 +1,21 @@
 export const projectList = [
   {
     id: 1,
-    image: "/img/project/pro1.png",
+    image: "/img/project/project1.png",
     heading: "Travel Buddy",
     subHeading: "Full-Stack Travel Planning Platform",
     category: ["all", "fullstack", "frontend", "backend"],
     liveSite: "https://travel-buddy-client-ten.vercel.app/",
-    clientRepo: "https://github.com/ashikuzzamanmoon/travel-buddy-client", 
-    serverRepo: "https://github.com/ashikuzzamanmoon/travel-buddy-server", 
+    clientRepo: "https://github.com/ashikuzzamanmoon/travel-buddy-client",
+    serverRepo: "https://github.com/ashikuzzamanmoon/travel-buddy-server",
     clientName: "Personal Project",
     projectDate: "October 2023",
-    description: "A full-featured platform for travelers to plan trips, find compatible travel partners, and manage their adventures. Features a robust back-end with Prisma and Express.js, and a comprehensive user dashboard with full CRUD functionality and secure, role-based access control using JWT.",
-    detailsImages: ["/img/protfolio/prot-detials1.png", "/img/protfolio/prot-detials2.png"]
+    description:
+      "A full-featured platform for travelers to plan trips, find compatible travel partners, and manage their adventures. Features a robust back-end with Prisma and Express.js, and a comprehensive user dashboard with full CRUD functionality and secure, role-based access control using JWT.",
+    detailsImages: [
+      "/img/protfolio/port-details-1-1.png",
+      "/img/protfolio/port-details-1-2.png",
+    ],
   },
   {
     id: 2,
@@ -19,27 +23,42 @@ export const projectList = [
     heading: "Chitro Golpo",
     subHeading: "E-Learning Platform for Photographers",
     category: ["all", "fullstack", "frontend", "backend"],
-    liveSite: "https://chitro-golpo.web.app/", 
-    clientRepo: "https://github.com/ashikuzzamanmoon/chitro-golpo-client", 
-    serverRepo: "https://github.com/ashikuzzamanmoon/chitro-golpo-server", 
+    liveSite: "https://chitro-golpo.web.app/",
+    clientRepo: "https://github.com/ashikuzzamanmoon/chitro-golpo-client",
+    serverRepo: "https://github.com/ashikuzzamanmoon/chitro-golpo-server",
     clientName: "Personal Project",
     projectDate: "September 2023",
-    description: "An e-learning platform where users can browse, purchase, and stream photography courses from instructors. Engineered a secure, role-based access system (Admin, Instructor, Student) using Firebase Authentication and JWT.",
-    detailsImages: ["/img/protfolio/prot-detials1.png", "/img/protfolio/prot-detials2.png"]
+    description:
+      "An e-learning platform where users can browse, purchase, and stream photography courses from instructors. Engineered a secure, role-based access system (Admin, Instructor, Student) using Firebase Authentication and JWT.",
+    detailsImages: [
+      "/img/protfolio/prot-detials1.png",
+      "/img/protfolio/prot-detials2.png",
+    ],
   },
   {
     id: 3,
     image: "/img/project/pro5.png",
     heading: "Digital Angan",
     subHeading: "WordPress ecommerce landing page",
-    category: ["all", "frontend", "website", "wordpress", "ecommerce", "landing_page"],
+    category: [
+      "all",
+      "frontend",
+      "website",
+      "wordpress",
+      "ecommerce",
+      "landing_page",
+    ],
     liveSite: "https://digitalangan.com",
     clientRepo: "#", // Not applicable for WordPress theme dev
     serverRepo: "#", // Not applicable for WordPress theme dev
     clientName: "Personal Project",
     projectDate: "August 2023",
-    description: "A responsive and visually appealing e-commerce landing page built with WordPress and modern design principles to showcase products effectively and drive conversions.",
-    detailsImages: ["/img/protfolio/prot-detials1.png", "/img/protfolio/prot-detials2.png"]
+    description:
+      "A responsive and visually appealing e-commerce landing page built with WordPress and modern design principles to showcase products effectively and drive conversions.",
+    detailsImages: [
+      "/img/protfolio/prot-detials1.png",
+      "/img/protfolio/prot-detials2.png",
+    ],
   },
   {
     id: 4,
@@ -52,8 +71,12 @@ export const projectList = [
     serverRepo: "#", // Not applicable
     clientName: "Practice Project",
     projectDate: "July 2023",
-    description: "A frontend clone of the popular e-commerce site Shajgoj, focusing on replicating the user interface and experience with pixel-perfect design and responsiveness.",
-    detailsImages: ["/img/protfolio/prot-detials1.png", "/img/protfolio/prot-detials2.png"]
+    description:
+      "A frontend clone of the popular e-commerce site Shajgoj, focusing on replicating the user interface and experience with pixel-perfect design and responsiveness.",
+    detailsImages: [
+      "/img/protfolio/prot-detials1.png",
+      "/img/protfolio/prot-detials2.png",
+    ],
   },
   {
     id: 5,
@@ -66,8 +89,12 @@ export const projectList = [
     serverRepo: "#", // Not applicable
     clientName: "Practice Project",
     projectDate: "June 2023",
-    description: "A frontend clone of the popular beauty product e-commerce site Sundora. This project showcases skills in building complex and beautiful user interfaces.",
-    detailsImages: ["/img/protfolio/prot-detials1.png", "/img/protfolio/prot-detials2.png"]
+    description:
+      "A frontend clone of the popular beauty product e-commerce site Sundora. This project showcases skills in building complex and beautiful user interfaces.",
+    detailsImages: [
+      "/img/protfolio/prot-detials1.png",
+      "/img/protfolio/prot-detials2.png",
+    ],
   },
   {
     id: 6,
@@ -75,13 +102,17 @@ export const projectList = [
     heading: "Mobile Expo",
     subHeading: "E-commerce Mobile App",
     category: ["all", "frontend", "website", "ecommerce"],
-    liveSite: "#", 
+    liveSite: "#",
     clientRepo: "#", // Add client repo link if available
     serverRepo: "#", // Add client repo link if available
     clientName: "Client Project",
     projectDate: "May 2023",
-    description: "A fully functional e-commerce application for a mobile phone shop, featuring product listings, a shopping cart, and a checkout process. Built with a focus on user experience.",
-    detailsImages: ["/img/protfolio/port-details-6-1.png", "/img/protfolio/port-details-6-1.png"]
+    description:
+      "A fully functional e-commerce application for a mobile phone shop, featuring product listings, a shopping cart, and a checkout process. Built with a focus on user experience.",
+    detailsImages: [
+      "/img/protfolio/port-details-6-1.png",
+      "/img/protfolio/port-details-6-1.png",
+    ],
   },
 ];
 
@@ -93,6 +124,5 @@ export const imagesList = [
   "/img/project/pro4.png",
   "/img/project/pro6.png",
 ];
-
 
 // export const imagesList = projectList.map(p => p.image);
